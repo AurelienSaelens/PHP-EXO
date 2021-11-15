040d53a5672f3d741342687075f235afe6a21db1
Aurelien Saelens <90911945+AurelienSaelens@users.noreply.github.com>
2021-11-16 00:56:10 +0100

    Update variable_types.php

--- variable/variable_types.php
+++ variable/variable_types.php
@@ -1,21 +1,24 @@
 <html>
   <head><title>Hi!</title></head>
-  <body>
-    <h1>Hello my name is <?php $_NAME = "Aurelien"; echo $_NAME;?></h1> 
-    <p>I am <?php $_AGE = 26; echo $_AGE;?></p>
-    <p>My eyes are <?php $_EYES = "brown"; echo $_EYES;?></p>
-    <p>The first person in my family is <?php 
-    $_FAMILY = array(
-        0 => 'Coralie', 
-        1 => 'Sylvie',   
-        2 => 'Emma'); echo $_FAMILY[1];?></p>
-    <?php
-        $hungry = true;
-        if ($hungry == true) {
-            echo "J'ai faim";
-        } else {
-            echo "Je n'ai pas faim";
-        }
-    ?>
-  </body>
+    <body>
+        <?php $_EYES = "brown";?>
+        <?php $_AGE = "26";?>
+        <?php $_NAME = "Aurelien";?>
+        <?php $_FAMILY = array(
+            0 => 'Coralie', 
+            1 => 'Sylvie',   
+            2 => 'Emma');
+        ?>
+        <h1>Hello my name is <?= $_NAME?></h1> 
+        <p>I am <?= $_AGE?></p>
+        <p>My eyes are <?= $_EYES?></p>
+        <p>The first person in my family is <?= $_FAMILY[0];?></p>
+        <?php $hungry = true;
+            if ($hungry == true) {
+                echo "J'ai faim";
+            } else {
+                echo "Je n'ai pas faim";
+            }
+        ?>
+    </body>
 </html>
