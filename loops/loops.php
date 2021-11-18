5fcd5f2225622451977f7d0508c8a73d5f0a3352
Aurelien Saelens <aureliensaelens@MacBook-Air-de-Aurelien.local>
2021-11-18 12:24:38 +0100

    loopsDone

---
+++ loops/loops.php
@@ -0,0 +1,63 @@
+<?php /*
+    $pronouns = array('I', 'You', 'He/She','We', 'You', 'They');
+
+    foreach($pronouns as $pronouns) {
+        if($pronouns == 'I'){
+            echo "$pronouns code <br>";}
+       elseif($pronouns == 'YOU'){
+            echo "$pronouns code <br>";}
+       elseif($pronouns == 'He/She'){
+            echo "$pronouns codes <br>";}
+       elseif($pronouns == 'We'){
+            echo "$pronouns code <br>";}
+       elseif($pronouns == 'You'){
+            echo "$pronouns code <br>";}
+       elseif($pronouns == 'They'){
+            echo "$pronouns code <br>";}
+    }
+*/ ?>
+<?php /*
+    $number_of_line = 1;
+    while ($number_of_line <= 100) {
+        echo "I shall not watch flies fly when I'm learning PHP <br>";
+    }
+    $number_of_line++;
+*/ ?>
+<?php /*
+    for ($amount_of_lines = 1; $amount_of_lines <= 100; $amount_of_lines ++)
+    {
+         echo "I shall not watch flies fly when I'm learning PHP <br>";
+    }
+*/?>
+
+<?php /*
+    for( $number=0; $number<=120; $number++){
+    echo $number;
+    echo "<br>";
+    }
+ */ ?>
+
+<?php
+$countries = array ('Belgium' => 'Belgium',
+                    'France' => 'France',
+                    'Germany' => 'Germany',
+                    'Danemark' => 'Danemark',
+                    'Sweden' => 'Sweden',
+                    'Poland' => 'Poland',
+                    'Italia' => 'Italia',
+                    'Spain' => 'Spain',
+                    'Portugal' => 'Portugal',
+                    'Netherlands' => 'Netherlands')
+?>
+
+<form action="loops.php" method="GET">
+  <select name="countries">
+    <?php
+   foreach($countries as $key => $value)
+   {
+   echo "<option value='$key'> $key</option>";
+   }
+?>
+  </select>
+  <button type="submit" name="envoyer">Envoyer</button>
+</form>
